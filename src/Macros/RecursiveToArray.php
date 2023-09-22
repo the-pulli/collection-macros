aac785e9b9782956e52cf84835daaf2a35b0b47a
PuLLi <112799107+the-pulli@users.noreply.github.com>
2023-09-22 08:55:52 +0200

    refactor: RecursiveToArray

--- src/Macros/RecursiveToArray.php
+++ src/Macros/RecursiveToArray.php
@@ -2,14 +2,27 @@
 
 namespace Pulli\CollectionMacros\Macros;
 
+use Closure;
 use Illuminate\Support\Collection;
 
 class RecursiveToArray
 {
-    public function __invoke(): \Closure
+    public function __invoke(): Closure
     {
         return function(array $ary): array {
-            return Collection::mapToCollection($ary, true)->toArray();
+            $closure = function (&$ary) {
+                if (is_array($ary)) {
+                    $ary = Collection::recursiveToArray($ary);
+                }
+
+                if (is_object($ary) && method_exists($ary, 'toArray')) {
+                    $ary = Collection::recursiveToArray($ary->toArray());
+                }
+            };
+
+            array_walk($ary, $closure);
+
+            return $ary;
         };
     }
 }
