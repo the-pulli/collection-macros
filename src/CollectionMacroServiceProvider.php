956bb02e068274fe42b8406b9b6c3184e0d75e7e
PuLLi <112799107+the-pulli@users.noreply.github.com>
2023-12-19 10:01:11 +0100

    Make ServiceProvider dynamic

--- src/CollectionMacroServiceProvider.php
+++ src/CollectionMacroServiceProvider.php
@@ -3,24 +3,27 @@
 namespace Pulli\CollectionMacros;
 
 use Illuminate\Support\Collection;
-use Illuminate\Support\ServiceProvider;
 
-class CollectionMacroServiceProvider extends ServiceProvider
-{
-    public function register(): void
+if (class_exists('\Illuminate\Support\ServiceProvider')) {
+    class CollectionMacroServiceProvider extends \Illuminate\Support\ServiceProvider
     {
-        Collection::make(self::macros())
-            ->reject(fn ($class, $macro) => Collection::hasMacro($macro))
-            ->each(fn ($class, $macro) => Collection::macro($macro, app($class)()));
-    }
+        public function register(): void
+        {
+            Collection::make(self::macros())
+                ->reject(fn ($class, $macro) => Collection::hasMacro($macro))
+                ->each(fn ($class, $macro) => Collection::macro($macro, app($class)()));
+        }
 
-    public static function macros(): array
-    {
-        return [
-            'mapToCollection' => \Pulli\CollectionMacros\Macros\MapToCollection::class,
-            'mapToCollectionFrom' => \Pulli\CollectionMacros\Macros\MapToCollectionFrom::class,
-            'recursiveToArray' => \Pulli\CollectionMacros\Macros\RecursiveToArray::class,
-            'recursiveToArrayFrom' => \Pulli\CollectionMacros\Macros\RecursiveToArrayFrom::class,
-        ];
+        public static function macros(): array
+        {
+            return [
+                'mapToCollection' => \Pulli\CollectionMacros\Macros\MapToCollection::class,
+                'mapToCollectionFrom' => \Pulli\CollectionMacros\Macros\MapToCollectionFrom::class,
+                'recursiveToArray' => \Pulli\CollectionMacros\Macros\RecursiveToArray::class,
+                'recursiveToArrayFrom' => \Pulli\CollectionMacros\Macros\RecursiveToArrayFrom::class,
+            ];
+        }
     }
+} else {
+    class CollectionMacroServiceProvider {}
 }
